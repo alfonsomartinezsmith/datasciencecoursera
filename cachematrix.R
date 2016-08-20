b6ab6754a8bc591f3ad506b0b10893d86426c993
alfonsomartinezsmith <alfonsomartinezsmith@users.noreply.github.com>
2016-08-20 09:40:54 -0500

    Add files via upload

---
+++ cachematrix.R
@@ -0,0 +1,29 @@
+## Code submission for R Programming, Assignment 2
+#  Test using the following line:
+#  	cacheSolve(makeCacheMatrix(matrix(1:4, nrow=2, ncol=2)))
+
+makeCacheMatrix <- function(x = matrix()) {
+     inv <- NULL
+     set <- function(y) {
+          x <<- y
+          inv <<- NULL
+     }
+     get <- function() x
+     setinv <- function(solve) inv <<- solve
+     getinv <- function() inv
+     list(set = set, get = get,
+          setinv = setinv,
+          getinv = getinv)
+}
+
+cacheSolve <- function(x, ...) {
+     inv <- x$getinv()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     data <- x$get()
+     inv <- solve(data, ...)
+     x$setinv(inv)
+     inv
+}
